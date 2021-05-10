4b1aae0f424e3dd0073a5d7253bb2aa5a06b8818
MarcosRychlewski <vinirychlewski@gmail.com>
2021-05-09 22:46:14 -0300

    erro

--- src/Xadrez/PartidaXadrez.java
+++ src/Xadrez/PartidaXadrez.java
@@ -33,8 +33,29 @@ public class PartidaXadrez {
 	
 	private void inicioPartida() {
 		colocarNovaPeça('b', 6, new Torre(tabuleiro, Cor.WHITE));
-		colocarNovaPeça('e', 8,new Rei(tabuleiro, Cor.BLACK));
-		colocarNovaPeça('e', 1,new Rei(tabuleiro, Cor.WHITE));
+		colocarNovaPeça('e', 8, new Rei(tabuleiro, Cor.BLACK));
+		colocarNovaPeça('e', 1, new Rei(tabuleiro, Cor.WHITE));
+		colocarNovaPeça('c', 1, new Torre(tabuleiro, Cor.WHITE));
+		colocarNovaPeça('c', 2, new Torre(tabuleiro, Cor.WHITE));
+		colocarNovaPeça('d', 2, new Torre(tabuleiro, Cor.WHITE));
+	    colocarNovaPeça('e', 2, new Torre(tabuleiro, Cor.WHITE));
+	    colocarNovaPeça('d', 1, new Rei(tabuleiro, Cor.WHITE));
+
+	    colocarNovaPeça('c', 7, new Rei(tabuleiro, Cor.BLACK));
+	    colocarNovaPeça('c', 7, new Torre(tabuleiro, Cor.BLACK));
+	    colocarNovaPeça('c', 8, new Torre(tabuleiro, Cor.BLACK));
+	    colocarNovaPeça('d', 7, new Torre(tabuleiro, Cor.BLACK));
+	    colocarNovaPeça('e', 7, new Torre(tabuleiro, Cor.BLACK));
+	    colocarNovaPeça('d', 8, new Rei(tabuleiro, Cor.BLACK));
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
